3290aac6080e32dbacc4076142bf9e02089a672f
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-02 10:42:35 -0500

    SYS_epoll_...

---
+++ linux/SYS_epoll_ctl.h
@@ -0,0 +1,5 @@
+#ifdef __x86_64__
+#  include "c/linux/x86_64/SYS_epoll_ctl.h"
+#else
+#  error todo
+#endif
---
+++ linux/x86_64/SYS_epoll_create1.h
@@ -0,0 +1,20 @@
+#ifndef _c__linux__x86_64__SYS_epoll_create1_h
+#define _c__linux__x86_64__SYS_epoll_create1_h
+
+#include "c/linux/x86_64/_SystemCallNumber.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+static const _SystemCallNumber SYS_epoll_create1 = 291;
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
