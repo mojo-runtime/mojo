8bdf8fc2636a7cebda537690e2f649f08fe49bfa
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-02 08:30:42 -0500

    x86_64: some platform-dependent, posix typedefs

---
+++ linux/x86_64/clockid_t.h
@@ -0,0 +1,20 @@
+#ifndef _c__linux__x86_64__clockid_t_h
+#define _c__linux__x86_64__clockid_t_h
+
+#include "c/int32_t.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+typedef int32_t clockid_t;
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
---
+++ linux/x86_64/mode_t.h
@@ -0,0 +1,20 @@
+#ifndef _c__linux__x86_64__mode_t_h
+#define _c__linux__x86_64__mode_t_h
+
+#include "c/uint32_t.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+typedef uint32_t mode_t;
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
