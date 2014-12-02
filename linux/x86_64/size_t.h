24af45107d83bb7efbc93a84251564b79bb5b50c
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-02 08:17:06 -0500

    x86_64: size_t, ssize_t

---
+++ linux/x86_64/size_t.h
@@ -0,0 +1,20 @@
+#ifndef _c__linux__x86_64__size_t_h
+#define _c__linux__x86_64__size_t_h
+
+#include "c/uint64_t.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+typedef uint64_t size_t;
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
