254f9363ebab8b12211a4fa50db70927e8caa233
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-02 12:33:12 -0500

    ... and do the same for compiler stuff.  The __builtin_... headers could be public in the future.

--- __compiler/__builtin_assume?.h
+++ __compiler/__builtin_assume?.h
@@ -1,5 +1,5 @@
 #ifndef __builtin_assume
-#  include "c/__has_builtin?.h"
+#  include "c/__compiler/__has_builtin?.h"
 #  if !__has_builtin(__builtin_assume)
 #    define __builtin_assume(x) ()
 #  endif
