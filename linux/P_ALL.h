6d1b1d4bd7de31b32d2b1b2997647abf9fef1244
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-02 09:30:13 -0500

    waitid P_xxx -- platform independent

---
+++ linux/P_ALL.h
@@ -0,0 +1,18 @@
+#ifndef _c__linux__P_ALL_h
+#define _c__linux__P_ALL_h
+
+#include "c/linux/_WaitIdType.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+#endif
+
+static const _WaitIdType P_ALL = 0;
+
+#ifdef __cplusplus
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
---
+++ linux/P_PID.h
@@ -0,0 +1,18 @@
+#ifndef _c__linux__P_PID_h
+#define _c__linux__P_PID_h
+
+#include "c/linux/_WaitidType.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+#endif
+
+static const _WaitidType P_PID = 1;
+
+#ifdef __cplusplus
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
