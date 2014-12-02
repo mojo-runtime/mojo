3b636b7b90670140c4e5062d054a7d7ade1672b4
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-02 09:54:33 -0500

    EPOLL_CTL_xxx, universal

---
+++ linux/EPOLL_CTL_ADD.h
@@ -0,0 +1,18 @@
+#ifndef _c__linux__EPOLL_CTL_ADD_h
+#define _c__linux__EPOLL_CTL_ADD_h
+
+#include "c/linux/_EpollCtlOperation.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+#endif
+
+static const _EpollCtlOperation EPOLL_CTL_ADD = 1;
+
+#ifdef __cplusplus
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
---
+++ linux/EPOLL_CTL_DEL.h
@@ -0,0 +1,18 @@
+#ifndef _c__linux__EPOLL_CTL_DEL_h
+#define _c__linux__EPOLL_CTL_DEL_h
+
+#include "c/linux/_EpollCtlOperation.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+#endif
+
+static const _EpollCtlOperation EPOLL_CTL_DEL = 1;
+
+#ifdef __cplusplus
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
