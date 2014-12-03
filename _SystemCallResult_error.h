86d2adc202e3b66c2089a01ae6139684320ae253
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-03 13:39:19 -0500

    _SystemCallResult: methods for retrieving error information

---
+++ _SystemCallResult_error.h
@@ -0,0 +1,5 @@
+#ifdef __linux__
+#  include "c/linux/_SystemCallResult_error.h"
+#else
+#  error todo
+#endif
---
+++ _SystemCallResult_is_error.h
@@ -0,0 +1,5 @@
+#ifdef __linux__
+#  include "c/linux/_SystemCallResult_is_error.h"
+#else
+#  error todo
+#endif
