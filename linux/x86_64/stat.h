07c7b5c998af085146d012ee3e057c37ea3c9424
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-03 13:21:08 -0500

    `stat` implemented

---
+++ linux/x86_64/stat.h
@@ -0,0 +1,90 @@
+#ifndef _c__linux__x86_64__stat_h
+#define _c__linux__x86_64__stat_h
+
+#include "c/linux/x86_64/blksize_t.h"
+#include "c/linux/x86_64/blkcnt_t.h"
+#include "c/linux/x86_64/dev_t.h"
+#include "c/linux/x86_64/gid_t.h"
+#include "c/linux/x86_64/ino_t.h"
+#include "c/linux/x86_64/mode_t.h"
+#include "c/linux/x86_64/nlink_t.h"
+#include "c/linux/x86_64/off_t.h"
+#include "c/linux/x86_64/time_t.h"
+#include "c/linux/x86_64/uid_t.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+struct stat
+{
+    // Gleened from both glibc and linux sources, the best being
+    // LINUX/arch/x86/include/uapi/asm/stat.h
+
+    dev_t
+    st_dev;
+
+    ino_t
+    st_ino;
+
+    nlink_t
+    st_nlink;
+
+    mode_t
+    st_mode;
+
+    uid_t
+    st_uid;
+
+    gid_t
+    st_gid;
+
+    uint32_t // unsigned int
+    __pad0;
+
+    dev_t
+    st_rdev;
+
+    off_t
+    st_size;
+
+    blksize_t
+    st_blksize;
+
+    blkcnt_t
+    st_blocks;
+
+    // timespecs - could be unions, but
+    // don't force that dependency, at least for now
+
+    time_t
+    st_atime;
+
+    uint64_t
+    st_atime_nsec;
+
+    time_t
+    st_mtime;
+
+    uint64_t
+    st_mtime_nsec;
+
+    time_t
+    st_ctime;
+
+    uint64_t
+    st_ctime_nsec;
+
+    uint64_t
+    __unused[3];
+};
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
