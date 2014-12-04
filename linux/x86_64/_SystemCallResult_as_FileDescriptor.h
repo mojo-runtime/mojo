140f045d2c174300fa393ebc7828ba97b9e19cdf
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-04 09:42:47 -0500

    _sys_close

---
+++ linux/x86_64/_SystemCallResult_as_FileDescriptor.h
@@ -0,0 +1,27 @@
+#ifndef _c__linux__x86_64___SystemCallResult_as_FileDescriptor_h
+#define _c__linux__x86_64___SystemCallResult_as_FileDescriptor_h
+
+#include "c/linux/x86_64/_FileDescriptor.h"
+#include "c/linux/x86_64/_SystemCallResult.h"
+#include "c/__internal/__c_STATIC_CAST.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+static
+_FileDescriptor
+_SystemCallResult_as_FileDescriptor(_SystemCallResult result)
+{
+    return __c_STATIC_CAST(_FileDescriptor, result.__word);
+}
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
