adcca72a61b838411f6c3131c0114c9d80ad60d2
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-04 09:59:20 -0500

    _sys_getdents

---
+++ linux/x86_64/SYS_getdents.h
@@ -0,0 +1,20 @@
+#ifndef _c__linux__x86_64__SYS_getdents_h
+#define _c__linux__x86_64__SYS_getdents_h
+
+#include "c/linux/x86_64/_SystemCallNumber.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+static const _SystemCallNumber SYS_getdents = 78;
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
