21ce7b5788e36089e658ae68bf9f00ca723b0099
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-04 15:20:54 -0500

    puts

---
+++ EOF.h
@@ -0,0 +1,14 @@
+#ifndef _c__EOF_h
+#define _c__EOF_h
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+static const int EOF = -1;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
---
+++ STDOUT_FILENO.h
@@ -0,0 +1,16 @@
+#ifndef _c__STDOUT_FILENO_h
+#define _c__STDOUT_FILENO_h
+
+#include "c/_FileDescriptor.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+static const _FileDescriptor STDOUT_FILENO = 1;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
