b1f4eb09ecf3e4f6fcd9709c1e62e7b55094536b
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-07 09:53:40 -0500

    _sys_fork

---
+++ _SystemCallResult_as_pid.h
@@ -0,0 +1,5 @@
+#ifdef __linux__
+#  include "c/linux/_SystemCallResult_as_pid.h"
+#else
+#  error todo
+#endif
---
+++ linux/x86_64/_SystemCallResult_as_pid.h
@@ -0,0 +1,27 @@
+#ifndef _c__linux__x86_64___SystemCallResult_as_pid_h
+#define _c__linux__x86_64___SystemCallResult_as_pid_h
+
+#include "c/linux/x86_64/pid_t.h"
+#include "c/linux/x86_64/_SystemCallResult.h"
+#include "c/__internal/__c_STATIC_CAST.h"
+
+#ifdef __cplusplus
+inline namespace c {
+inline namespace linux {
+inline namespace x86_64 {
+#endif
+
+static
+pid_t
+_SystemCallResult_as_pid(_SystemCallResult result)
+{
+    return __c_STATIC_CAST(pid_t, result.__word);
+}
+
+#ifdef __cplusplus
+} // inline namespace x86_64
+} // inline namespace linux
+} // inline namespace c
+#endif
+
+#endif
