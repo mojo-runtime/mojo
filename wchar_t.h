53cfa8f4bf34c20a1b60961e7986090c377a06ec
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-19 15:58:05 -0500

    wchar_t

---
+++ wchar_t.h
@@ -0,0 +1,16 @@
+#ifndef _c__wchar_t__h
+#define _c__wchar_t__h
+
+#include "builtin/__WCHAR_TYPE__.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+typedef __WCHAR_TYPE__ wchar_t;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
