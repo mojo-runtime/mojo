15f4961f647cb86da69f6d6369d33e21b54494c8
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-19 15:42:07 -0500

    abs (trivial)

---
+++ abs.h
@@ -0,0 +1,21 @@
+#ifndef _c__abs_h
+#define _c__abs_h
+
+#include "c/__internal/__c_CONSTEXPR.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+static __c_CONSTEXPR
+int
+abs(int x)
+{
+    return x < 0 ? -x : x;
+}
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
