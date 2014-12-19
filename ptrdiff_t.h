4360558dc9717e79eb2f49f26619ea8f1b029f50
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-19 16:40:38 -0500

    Implement ANSI types (read: non-POSIX) using the builtin macros

--- ptrdiff_t.h
+++ ptrdiff_t.h
@@ -1,5 +1,16 @@
-#ifdef __linux__
-#  include "c/linux/ptrdiff_t.h"
-#else
-#  error todo
+#ifndef _c__ptrdiff_t__h
+#define _c__ptrdiff_t__h
+
+#include "builtin/__PTRDIFF_TYPE__.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
 #endif
--- size_t.h
+++ size_t.h
@@ -1,5 +1,16 @@
-#ifdef __linux__
-#  include "c/linux/size_t.h"
-#else
-#  error todo
+#ifndef _c__size_t__h
+#define _c__size_t__h
+
+#include "builtin/__SIZE_TYPE__.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+typedef __SIZE_TYPE__ size_t;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
 #endif
