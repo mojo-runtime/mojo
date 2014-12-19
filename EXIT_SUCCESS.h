c4b3ee0d860e2bd3f75d553beab95456c81f4565
Blake Sheridan <blake.m.sheridan@gmail.com>
2014-12-19 15:58:20 -0500

    EXIT_SUCCESS

---
+++ EXIT_SUCCESS.h
@@ -0,0 +1,16 @@
+#ifndef _c__EXIT_SUCCESS_h
+#define _c__EXIT_SUCCESS_h
+
+#include "c/_ExitStatus.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+static const _ExitStatus EXIT_SUCCESS = 0;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
