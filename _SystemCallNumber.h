b5bddc8e92b6671864b4f5a1d4e9b0a41fa05c84
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-01-29 10:55:27 -0500

    Accidentally deleted

---
+++ _SystemCallNumber.h
@@ -0,0 +1,14 @@
+#ifndef _c___SystemCallNumber_h
+#define _c___SystemCallNumber_h
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+typedef int _SystemCallNumber;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
