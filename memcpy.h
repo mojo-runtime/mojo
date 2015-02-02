2c8137c9f677bf56472829ed334af9b0b02d4307
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-02 11:20:52 -0500

    memcpy placeholder

---
+++ memcpy.h
@@ -0,0 +1,43 @@
+#ifndef _c__memcpy_h
+#define _c__memcpy_h
+
+#include "c/size_t.h"
+#include "c/_REINTERPRET_CAST.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+static
+void
+_memcpy(void* target, const void* source, size_t bytes)
+{
+    // Placeholder implementation
+
+    char*
+    target_bytes = _REINTERPRET_CAST(char*, target);
+
+    const char*
+    source_bytes = _REINTERPRET_CAST(const char*, source);
+
+    char*
+    target_bytes_end = target_bytes + bytes;
+
+    while (target_bytes != target_bytes_end) {
+        *target_bytes++ = *source_bytes++;
+    }
+}
+
+static
+void*
+memcpy(void* target, const void* source, size_t bytes)
+{
+    _memcpy(target, source, bytes);
+    return target;
+}
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
