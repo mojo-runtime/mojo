18103b94bd89fad5d8b01d95f5c18120a5dda310
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-13 17:09:14 -0500

    STDIN_FILENO

---
+++ STDIN_FILENO.h
@@ -0,0 +1,14 @@
+#ifndef c_STDIN_FILENO_h_
+#define c_STDIN_FILENO_h_
+
+#include "c/FileDescriptor.h"
+
+__c_namespace_open
+
+static
+const FileDescriptor
+STDIN_FILENO = 0;
+
+__c_namespace_close
+
+#endif
