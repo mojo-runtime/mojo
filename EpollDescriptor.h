b1d2a43d1789eb0ae1430479c26c0fba799104ef
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-12 19:58:09 -0500

    EpollDescriptor

---
+++ EpollDescriptor.h
@@ -0,0 +1,16 @@
+#ifndef c_EpollDescriptor_h_
+#define c_EpollDescriptor_h_
+
+#include "c/FileDescriptor.h"
+
+#ifdef __cplusplus
+inline namespace c {
+#endif
+
+typedef FileDescriptor EpollDescriptor;
+
+#ifdef __cplusplus
+} // inline namespace c
+#endif
+
+#endif
