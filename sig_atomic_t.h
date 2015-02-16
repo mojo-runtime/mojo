32aee2e75fa3fc6a5a8fc086e59593f56e3be501
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-15 19:02:41 -0500

    … and add `sig_atomic_t`

---
+++ sig_atomic_t.h
@@ -0,0 +1,13 @@
+#ifndef c_sig_atomic_t_h_
+#define c_sig_atomic_t_h_
+
+#include "builtin/__SIG_ATOMIC_TYPE__.h"
+#include "c/__c_namespace.h"
+
+__c_namespace_open
+
+typedef __SIG_ATOMIC_TYPE__ sig_atomic_t;
+
+__c_namespace_close
+
+#endif
