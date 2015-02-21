f5b77d8a67611b1e69a662663c42692f9098048f
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-21 12:05:05 -0500

    SYS_clock_getres

---
+++ SYS_clock_getres.h
@@ -0,0 +1,12 @@
+#ifndef c_SYS_clock_getres_h_
+#define c_SYS_clock_getres_h_
+
+#include "_define/SYS_.h"
+
+#if defined(__linux__) && defined(__x86_64__)
+_c_define_SYS_(clock_getres, 229);
+#else
+#  error
+#endif
+
+#endif
