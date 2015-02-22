a53b310dc4ecdc64c46810e32cdc19fe9312a9f8
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-21 22:39:41 -0500

    Unnecessary artifact

--- _c_noreturn.h
+++ _c_noreturn.h
@@ -1,10 +1,6 @@
 #ifndef _c_noreturn
 #  if defined(__cplusplus) && __cplusplus >= 201103L
-#    define _c_noreturn \
-    _Pragma("clang diagnostic push") \
-    _Pragma("clang diagnostic ignored \"-Wdisabled-macro-expansion\"") \
-    [[noreturn]] \
-    _Pragma("clang diagnostic pop")
+#    define _c_noreturn [[noreturn]]
 #  elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 #    define _c_noreturn _Noreturn
 #  elif defined(__GNUC__)
