9160f55bf20d7d90ec7c72c7fc9430e87877d841
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-22 06:08:16 -0500

    _exit: use the syscall macro

--- _exit.h
+++ _exit.h
@@ -1,26 +1,18 @@
 #ifndef c_exit_h_
 #define c_exit_h_
 
+#include "SYS_exit.h"
 #include "extension/x_noexcept.h"
 #include "extension/x_noreturn.h"
 #include "extension/x_unreachable.h"
-
-#include "SYS_exit.h"
+#include "extension/x_syscall1.h"
 
 x_noreturn
 static
 void
 _exit(int status) x_noexcept
 {
-#if defined(__linux__) && defined(__x86_64__)
-    register int r1 __asm__ ("rdi") = status;
-
-    __asm__ __volatile__ ("syscall"
-                          :
-                          : "ra" (SYS_exit), "r" (r1));
-#else
-#  error
-#endif
+    x_syscall1(SYS_exit, status);
     x_unreachable();
 }
 
