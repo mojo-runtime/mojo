e3b3d58f410ec374c862605bcccdbabbc48c44f7
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-23 18:22:03 -0500

    The last 2 C11 features

---
+++ c/generic_selections.h
@@ -0,0 +1,7 @@
+#ifndef has_c_generic_selections
+#  ifdef __has_feature
+#    define has_c_generic_selections __has_feature(c_generic_selections)
+#  else
+#    error todo
+#  endif
+#endif
---
+++ c/thread_local.h
@@ -0,0 +1,7 @@
+#ifndef has_c_thread_local
+#  ifdef __has_feature
+#    define has_c_thread_local __has_feature(c_thread_local)
+#  else
+#    error todo
+#  endif
+#endif
