076cf3114cbfba99c70b325f5c02bf9f538c84c7
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-23 18:39:29 -0500

    GCC exception detection

--- cxx/exceptions.h
+++ cxx/exceptions.h
@@ -1,6 +1,12 @@
 #ifndef has_cxx_exceptions
 #  ifdef __has_feature
 #    define has_cxx_exceptions __has_feature(cxx_exceptions)
+#  elif defined (__GNUC__)
+#    if defined (__EXCEPTIONS)
+#      define has_cxx_exception 1
+#    else
+#      define has_cxx_exception 0
+#    endif
 #  else
 #    error todo
 #  endif
