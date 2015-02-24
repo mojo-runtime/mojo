a4ba62d5e1848a5c04c4320e4ebce51868f98e8e
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 13:49:33 -0500

    Get <stdbool.h> right

--- stdbool.h
+++ stdbool.h
@@ -1,18 +1,11 @@
 #ifndef _stdbool_h_
 #define _stdbool_h_
 
-// Per: http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stdbool.h.html
+// All three of these perform the (reentrant) task of <stdbool.h>.
+// See: http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stdbool.h.html
 
-//------------------------------------------------------------------------------------------------//
-// "The <stdbool.h> header shall define the following macros:"
-
-#include "c/bool.h"  // "Expands to `_Bool`."
-#include "c/true.h"  // "Expands to the integer constant 1."
-#include "c/false.h" // "Expands to the integer constant 0."
-
-// TODO(?): __bool_true_and_false_are_defined
-// "Expands to the integer constant 1."
-
-//------------------------------------------------------------------------------------------------//
+#include "c/bool.h"
+//#include "c/true.h"
+//#include "c/false.h"
 
 #endif
