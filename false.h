c60c03426affdd36a84c1c63b6444bdaad1111bc
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 14:01:18 -0500

    false/true: delegate to bool

--- false.h
+++ false.h
@@ -1,8 +1,1 @@
-#ifndef __bool_true_false_are_defined
-#define __bool_true_false_are_defined 1
-#  ifndef __cplusplus
-#    define bool _Bool
-#    define true 1
-#    define false 0
-#  endif
-#endif
+#include "bool.h" /* package deal */
