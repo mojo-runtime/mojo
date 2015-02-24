635f99624eb3641480dc2eb8eeb4a75cccee1497
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 13:27:01 -0500

    align(as|of): use feature checking, and follow the <stdalign.h> __alignxx_is_defined rule

--- alignas.h
+++ alignas.h
@@ -1,9 +1,16 @@
-#ifndef alignas
-#  if defined(__cplusplus) && __cplusplus >= 201103L
-//
-#  elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-#    define alignas(x) _Alignas(x)
+#ifndef __alignas_is_defined
+#  if defined __cplusplus
+#    include "has/cxx/alignas.h"
+#    if !has_cxx_alignas
+#      error
+#    endif
 #  else
-#    error
+#    include "has/c/alignas.h"
+#    if has_c_alignas
+#      define alignas _Alignas
+#    else
+#      error
+#    endif
 #  endif
+#  define __alignas_is_defined
 #endif
--- alignof.h
+++ alignof.h
@@ -1,9 +1,16 @@
-#ifndef alignof
-#  if defined(__cplusplus) && __cplusplus >= 201103L
-//
-#  elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-#    define alignof(x) _Alignof(x)
+#ifndef __alignof_is_defined
+#  if defined __cplusplus
+#    include "has/cxx/alignof.h"
+#    if has_cxx_alignof
+#      error
+#    endif
 #  else
-#    error
+#    include "has/c/alignof.h"
+#    if has_c_alignof
+#      define alignof _Alignof
+#    else
+#      error
+#    endif
 #  endif
+#  define __alignof_is_defined
 #endif
