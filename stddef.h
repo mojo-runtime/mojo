bbe3cf37733a2403f5406adc1a5035c11abb57b4
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 14:25:18 -0500

    Consistent header guard

--- stddef.h
+++ stddef.h
@@ -1,5 +1,5 @@
-#ifndef c_standard_stddef_h_
-#define c_standard_stddef_h_
+#ifndef _stddef_h_
+#define _stddef_h_
 
 // See: http://flash-gordon.me.uk/ansi.c.txt,
 // "4.1.5 Common definitions <stddef.h>"
