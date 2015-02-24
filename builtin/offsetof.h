ddbfeb60aa1ab97342d4c1c7e52e4d6cd12904ff
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 12:26:09 -0500

    has_builtin_offset workaround

--- builtin/offsetof.h
+++ builtin/offsetof.h
@@ -1,7 +1,8 @@
 #ifndef has_builtin_offsetof
-#  ifdef __has_builtin
-#    define has_builtin_offsetof __has_builtin(__builtin_offsetof)
-#  elif defined __GNUC__
+// Workaround: clang (3.6.0) returns false here
+// ifdef __has_builtin
+//    define has_builtin_offsetof __has_builtin(__builtin_offsetof)
+#  if defined __GNUC__
 #    define has_builtin_offsetof 1 /* assuming */
 #  else
 #    error todo
