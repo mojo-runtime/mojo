b1d8cd827ee67730e58477d55a520d280d5be92f
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 19:57:17 -0500

    Moved to abi/

--- ieee754/float.h
+++
@@ -1,9 +0,0 @@
-#ifndef compiler_has_ieee754_float
-#define compiler_has_ieee754_float                            \
-    (__FLT_DIG__        ==    6) &&                  \
-    (__FLT_MANT_DIG__   ==   24) &&                  \
-    (__FLT_MAX_10_EXP__ ==   38) &&                  \
-    (__FLT_MAX_EXP__    ==  128) &&                  \
-    (__FLT_MIN_10_EXP__ ==  -37) &&                  \
-    (__FLT_MIN_EXP__    == -125)
-#endif
