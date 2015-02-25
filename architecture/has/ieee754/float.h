15eac8c4b31189941a07cc627f04d4f56fda55f9
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-25 15:00:36 -0500

    abi_has_ieee… → architecture_has_ieee…

--- architecture/has/ieee754/float.h
+++ architecture/has/ieee754/float.h
@@ -1,5 +1,5 @@
-#ifndef abi_has_ieee754_float
-#define abi_has_ieee754_float                        \
+#ifndef architecture_has_ieee754_float
+#define architecture_has_ieee754_float               \
     (__FLT_DIG__        ==    6) &&                  \
     (__FLT_MANT_DIG__   ==   24) &&                  \
     (__FLT_MAX_10_EXP__ ==   38) &&                  \
