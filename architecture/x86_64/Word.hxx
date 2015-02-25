2e5847795de527a86aa60ff835aaf40d65b7d895
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 21:13:04 -0500

    Need not be private

--- architecture/x86_64/Word.hxx
+++ architecture/x86_64/Word.hxx
@@ -2,11 +2,11 @@
 #define architecture_x86_64_Word_hxx_
 
 namespace architecture {
-inline namespace _x86_64 {
+inline namespace x86_64 {
 
 using Word = unsigned long;
 
-} // inline namespace _x86_64
+} // inline namespace x86_64
 } // namespace architecture
 
 #endif
