8f88a22a08168f34b9357c3b2e3a7dff660585d3
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 19:56:53 -0500

    abi/has/ieee754/...

---
+++ has/ieee754/double.h
@@ -0,0 +1,9 @@
+#ifndef abi_has_ieee754_double
+#define abi_has_ieee754_double                       \
+    (__DBL_DIG__        ==    15) &&                 \
+    (__DBL_MANT_DIG__   ==    53) &&                 \
+    (__DBL_MAX_10_EXP__ ==   308) &&                 \
+    (__DBL_MAX_EXP__    ==  1024) &&                 \
+    (__DBL_MIN_10_EXP__ ==  -307) &&                 \
+    (__DBL_MIN_EXP__    == -1021)
+#endif
