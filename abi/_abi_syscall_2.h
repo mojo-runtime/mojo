75b62978f37f220686aa84bbf0435e7769ca9fae
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-24 20:56:15 -0500

    Encapsulation

--- abi/_abi_syscall_2.h
+++ abi/_abi_syscall_2.h
@@ -1,5 +1,5 @@
 #if defined(__x86_64__) && defined(__linux__)
-#  include "x86_64/linux/_abi_syscall_2.h"
+#  include "_x86_64/linux/_abi_syscall_2.h"
 #else
 #  error
 #endif
--- abi/_abi_syscall_3.h
+++ abi/_abi_syscall_3.h
@@ -1,5 +1,5 @@
 #if defined(__x86_64__) && defined(__linux__)
-#  include "x86_64/linux/_abi_syscall_3.h"
+#  include "_x86_64/linux/_abi_syscall_3.h"
 #else
 #  error
 #endif
