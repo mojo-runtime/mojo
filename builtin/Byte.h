1dadaa44e3f021e5ed208863fd683a4cbdcac56b
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-26 16:28:23 -0500

    Byte

---
+++ builtin/Byte.h
@@ -0,0 +1,6 @@
+#ifndef builtin_Byte_h_
+#define builtin_Byte_h_
+
+typedef unsigned int Byte __attribute__((__mode__(byte)));
+
+#endif
--- platform/__all__.h
+++ platform/__all__.h
@@ -1,6 +1,5 @@
 #include "has/__all__.h"
 
 #ifdef __cplusplus
-#include "Byte.hxx"
 #include "Word.hxx"
 #endif
