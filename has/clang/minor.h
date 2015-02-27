6479160e1d666a8f98b3d24063744d1eb5f43b5c
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-26 19:49:42 -0500

    has_clang_minor: was incorrect.  See has_gnuc_minor

--- has/clang/minor.h
+++ has/clang/minor.h
@@ -1,6 +1,6 @@
 #ifndef has_clang_minor
 #  if defined(__clang__)
-#    define has_clang_minor(x, y) (__clang_major__ >= (x) && __clang_minor__ >= (y))
+#    define has_clang_minor(x, y) ((__clang_major__ << 16) + __clang_minor__ >= ((x) << 16) + (y))
 #  else
 #    define has_clang_minor(x, y) 0
 #  endif
