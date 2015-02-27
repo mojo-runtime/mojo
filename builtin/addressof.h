ac68f8c19735e5fdc3f2a4774354a4da3e5857f4
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-27 18:41:42 -0500

    addressof(): trivial C version

--- builtin/addressof.h
+++ builtin/addressof.h
@@ -1,12 +1,15 @@
 #ifndef c_builtin_addressof_h_
 #define c_builtin_addressof_h_
 
-#include "config/has_builtin_addressof.h"
-
-#if has_builtin_addressof
-#  define addressof __builtin_addressof
+#if !defined(__cplusplus)
+#  define addressof(x) (&(x))
 #else
-#  error
+#  include "config/has_builtin_addressof.h"
+#  if has_builtin_addressof
+#    define addressof __builtin_addressof
+#  else
+#    error
+#  endif
 #endif
 
 #endif
