bbff5bd95b3fe25ba02ad69e7dc505b6b9abf16d
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-27 17:08:02 -0500

    has_attribute_: remove ones we do not provide

--- has/attribute/transparent_union.h
+++
@@ -1,9 +0,0 @@
-#ifndef has_attribute_transparent_union
-#  ifdef __has_attribute
-#    define has_attribute_transparent_union __has_attribute(__transparent_union__)
-#  elif defined __GNUC__
-#    define has_attribute_transparent_union 1 /* assuming */
-#  else
-#    error todo
-#  endif
-#endif
--- has/attribute/unused.h
+++
@@ -1,9 +0,0 @@
-#ifndef has_attribute_unused
-#  ifdef __has_attribute
-#    define has_attribute_unused __has_attribute(__unused__)
-#  elif defined __GNUC__
-#    define has_attribute_unused 1 /* assuming */
-#  else
-#    error todo
-#  endif
-#endif
