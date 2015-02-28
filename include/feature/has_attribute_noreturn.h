2917eb6f1455a9d8d60ec4b28d6f6b6adaa81465
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-02-28 03:14:19 -0500

    has_attribute_noreturn: gcc version

--- include/feature/has_attribute_noreturn.h
+++ include/feature/has_attribute_noreturn.h
@@ -2,8 +2,9 @@
 
 #if defined(__has_attribute)
 #  define has_attribute_noreturn (__has_attribute(__noreturn__))
-#elif defined(__GNUC__) // TODO: version
-#  define has_attribute_noreturn 1
+#elif defined(__GNUC__)
+#  include "_has_gnuc_minor.h"
+#  define has_attribute_noreturn _has_gnuc_minor(2, 5)
 #else
 #  error
 #endif
