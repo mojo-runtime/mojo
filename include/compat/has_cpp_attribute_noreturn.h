9c2d95e94f3c7d74b4cb9425e7266f89b41149c9
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-11 10:29:43 -0400

    Another clang false negative

--- include/compat/has_cpp_attribute_noreturn.h
+++ include/compat/has_cpp_attribute_noreturn.h
@@ -1,7 +1,8 @@
 #ifndef has_cpp_attribute_noreturn
 
-#if defined(__has_cpp_attribute)
-#  define has_cpp_attribute_noreturn __has_cpp_attribute(__noreturn__)
+#if defined(__clang__)
+// Workaround: __has_cpp_attribute(__noreturn__) → false negative (3.6.0)
+#  define has_cpp_attribute_noreturn 1
 #elif defined(__GNUC__)
 #  include "_has_gnuc_minor.h"
 #  define has_cpp_attribute_noreturn _has_gnuc_minor(4, 8)
