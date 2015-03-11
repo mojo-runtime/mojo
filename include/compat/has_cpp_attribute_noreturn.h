48af36ec5e43f8ea4b99e5771b475450cc6c0ed1
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-11 10:35:20 -0400

    Clang < 3.7 here

--- include/compat/has_cpp_attribute_noreturn.h
+++ include/compat/has_cpp_attribute_noreturn.h
@@ -1,8 +1,10 @@
 #ifndef has_cpp_attribute_noreturn
 
-#if defined(__clang__)
-// Workaround: __has_cpp_attribute(__noreturn__) → false negative (3.6.0)
-#  define has_cpp_attribute_noreturn 1
+#if defined(__has_cpp_attribute)
+#  define has_cpp_attribute_noreturn __has_cpp_attribute(__noreturn__)
+#elif defined(__clang__) // < 3.7
+#  include "_has_clang_minor.h"
+#  define has_cpp_attribute_noreturn _has_clang_minor(3, 3)
 #elif defined(__GNUC__)
 #  include "_has_gnuc_minor.h"
 #  define has_cpp_attribute_noreturn _has_gnuc_minor(4, 8)
