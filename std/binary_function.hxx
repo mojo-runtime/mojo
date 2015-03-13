c34dfa455968889a939640f418d281699dfbf960
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-13 06:29:09 -0400

    std::binary_function

---
+++ std/binary_function.hxx
@@ -0,0 +1,12 @@
+#ifndef _std_binary_function_hxx_
+#define _std_binary_function_hxx_
+
+namespace std {
+
+// Until C++17
+template <typename Arg1, typename Arg2, typename Result>
+struct binary_function;
+
+} // namespace std
+
+#endif
