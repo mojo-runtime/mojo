31e678dd708057b27fda9d564398caa301296e37
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-13 06:28:16 -0400

    std::unary_function

---
+++ std/unary_function.hxx
@@ -0,0 +1,12 @@
+#ifndef _std_unary_function_hxx_
+#define _std_unary_function_hxx_
+
+namespace std {
+
+// Until C++17
+template <typename ArgumentType, typename ResultType>
+struct unary_function;
+
+} // namespace std
+
+#endif
