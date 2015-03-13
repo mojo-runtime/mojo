2968eb79b9ccdb4bad7200363092287ab63ae932
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-13 16:05:07 -0400

    -Wc++98-compat-pedantic

--- c/std/lldiv_t.h
+++ c/std/lldiv_t.h
@@ -5,12 +5,16 @@
 
 typedef struct
 {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++98-compat-pedantic" // long long
     long long
     quot;
 
     long long
     rem;
+#pragma clang diagnostic pop
 }
 lldiv_t;
+
 
 #endif
