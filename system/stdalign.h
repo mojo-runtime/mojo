50df7fd7da4f0397f9a0e5d720bae55135e18ecd
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-13 00:18:54 -0400

    <std(align|bool).h>: do not bother with their silly functionality in c/

--- system/stdalign.h
+++ system/stdalign.h
@@ -1,9 +1,9 @@
 #ifndef __alignas_is_defined
-#include "c/alignas.h"
+#include "compat/alignas.h"
 #define __alignas_is_defined 1
 #endif
 
 #ifndef __alignof_is_defined
-#include "c/alignof.h"
+#include "compat/alignof.h"
 #define __alignof_is_defined 1
 #endif
