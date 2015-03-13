3240c2a2ca475b37578ae170a9f8aff37e3cae32
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-13 05:30:51 -0400

    Experimental branch not worried about co-existing with any standard library implementation

---
+++ __system/limits.h
@@ -0,0 +1,31 @@
+#ifndef _limits_h_
+#define _limits_h_
+
+// http://en.cppreference.com/w/c/types/limits
+
+#include "../c/CHAR_BIT.h"
+#include "../c/MB_LEN_MAX.h"
+#include "../c/CHAR_MIN.h"
+#include "../c/CHAR_MAX.h"
+
+#if 0 // C99
+#  include "../c/SCHAR_MIN.h"
+#  include "../c/SHRT_MIN.h"
+#  include "../c/INT_MIN.h"
+#  include "../c/LONG_MIN.h"
+#  include "../c/LLONG_MIN.h"
+//
+#  include "../c/SCHAR_MAX.h"
+#  include "../c/SHRT_MAX.h"
+#  include "../c/INT_MAX.h"
+#  include "../c/LONG_MAX.h"
+#  include "../c/LLONG_MAX.h"
+//
+#  include "../c/UCHAR_MAX.h"
+#  include "../c/USHRT_MAX.h"
+#  include "../c/UINT_MAX.h"
+#  include "../c/ULONG_MAX.h"
+#  include "../c/ULLONG_MAX.h"
+#endif
+
+#endif
