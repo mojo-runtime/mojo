71953e4356ccea3e0b278a07ee38292893976a7d
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-13 20:13:56 -0400

    `offset` can do the lifting

--- c/std/offsetof.h
+++ c/std/offsetof.h
@@ -1,7 +1,17 @@
 #ifndef offsetof
 
-#include "compat/__builtin_offsetof.h"
-
-#define offsetof __builtin_offsetof
+#if defined(__has_builtin)
+#  if __has_builtin(__builtin_offsetof)
+#    define offsetof __builtin_offsetof
+#  elif defined(__clang__) // False negative
+#    define offsetof __builtin_offsetof
+#  else
+#    error
+#  endif
+#elif defined(__GNUC__) // Version?
+#  define offsetof __builtin_offsetof
+#else
+#  error
+#endif
 
 #endif
