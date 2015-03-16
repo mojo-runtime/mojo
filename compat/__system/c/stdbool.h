cb925ecbf7defff0972e1b2135f01208f2cf1dd4
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-16 11:49:41 -0400

    Testing these without system header error suppression now

--- compat/__system/c/stdbool.h
+++ compat/__system/c/stdbool.h
@@ -6,4 +6,7 @@
 #  define true 1
 #endif
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
 #define __bool_true_false_are_defined 1
+#pragma clang diagnostic pop
