ea3907759fa97464cfea9b1a9ea000acd4e1be54
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-16 12:53:15 -0400

    c/noreturn: defer to compat/

--- c/noreturn.h
+++ c/noreturn.h
@@ -1,10 +1,6 @@
 #pragma once
 
 #if !defined(__cplusplus)
-#  include "compat/has_c_noreturn.h"
-#  if has_c_noreturn
-#    define noreturn _Noreturn
-#  else
-#    error
-#  endif
+#  include "compat/__noreturn.h"
+#  define noreturn _Noreturn
 #endif
