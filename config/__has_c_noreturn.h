c84153a64e46f88a1835c97ac4599a605031dd16
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-16 19:47:47 -0400

    __has_c_noreturn (missing)

---
+++ config/__has_c_noreturn.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+
+#if !defined(__has_c_noreturn) && defined(__has_feature)
+#  define __has_c_noreturn __has_feature(c_noreturn)
+#endif
+
+#if !defined(__has_c_noreturn) && defined(__GNUC__)
+#  include "__has_gnuc.h"
+#  define __has_c_noreturn __has_gnuc(4, 7)
+#endif
+
+#if !defined(__has_c_noreturn)
+#  define __has_c_noreturn 0
+#endif
+
+#pragma clang diagnostic pop
