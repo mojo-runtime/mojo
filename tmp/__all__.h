b1c4fd560aaa5ade2565bdbd5b88e148c0cdb16e
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-19 08:06:57 -0400

    Not sure when this got placed here

--- tmp/__all__.h
+++
@@ -1,8 +0,0 @@
-// Avoiding "__all__.h" in -isystem paths
-
-#include "c/limits.h"
-#include "c/stdalign.h"
-#include "c/stdarg.h"
-#include "c/stdbool.h"
-#include "c/stdint.h"
-#include "c/stdnoreturn.h"
