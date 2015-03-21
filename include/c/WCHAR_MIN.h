d37a2acc37cb0c4eeae8040114fb7a4f5738b8dd
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-21 09:15:14 -0400

    Mirror WINT_MIN, which accounts for ARM

--- include/c/WCHAR_MIN.h
+++ include/c/WCHAR_MIN.h
@@ -4,10 +4,16 @@
 
 #if defined(__WCHAR_MIN__)
 #  define WCHAR_MIN __WCHAR_MIN__
-#elif defined(__WCHAR_WIDTH__)
-#  include "WCHAR_MAX.h"
-#  if WCHAR_MAX == 2147483647
-#    define WCHAR_MIN (-2147483648)
+#elif defined(__GNUC__)
+#  if defined(__WCHAR_UNSIGNED__)
+#    define WCHAR_MIN (0u)
+#  elif defined(__SIZEOF_WCHAR_T__)
+#    if __SIZEOF_WCHAR_T__ == 4
+#      include "INT32_MIN.h"
+#      define WCHAR_MIN INT32_MIN
+#    else
+#      error
+#    endif
 #  else
 #    error
 #  endif
