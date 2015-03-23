e57badb7a40aec6deba51a5e9024f8a55875ea4c
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-23 09:23:35 -0400

    alloca(): macro, as to not contribute a stack frame

--- include/c/alloca.h
+++ include/c/alloca.h
@@ -2,19 +2,14 @@
 
 #include "size_t.h"
 
-static inline
-void*
-alloca(size_t size)
-{
 #if defined(__has_builtin)
 #  if __has_builtin(__builtin_alloca)
-    return __builtin_alloca(size);
+#    define alloca(size) __builtin_alloca(size)
 #  else
 #    error
 #  endif
 #elif defined(__GNUC__) // Version?
-    return __builtin_alloca(size);
+#  define alloca(size) __builtin_alloca(size)
 #else
 #  error
 #endif
-}
