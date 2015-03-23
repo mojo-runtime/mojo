e3b4c8a6106ad424c7f52c8fee51b54ad0e472d3
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-22 20:14:42 -0400

    Tidier assert()

--- include/c/assert.h
+++ include/c/assert.h
@@ -10,14 +10,39 @@
 #    define assert(...)
 #  endif
 #else
-#  include "__assertion_error.h"
-#  include "__va_dispatch!.h"
-#  define assert(...) __va_dispatch(assert_, __VA_ARGS__)
-#  if defined(__cplusplus)
-#    define assert_1(x) assert_2(x, nullptr)
-#    define assert_2(x, message)                  \
-    ((x) ? static_cast<void>(0) : __assertion_error(#x, __FILE__, __PRETTY_FUNCTION__, __LINE__, message))
-#  else
-#    error
-#  endif
+
+//--------------------------------------------------------------------------------------------------
+
+#include "EXIT_FAILURE.h"
+#include "_Exit.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+
+#define assert(...)               __assert_(__VA_ARGS__, 2, 1)(__VA_ARGS__)
+#define __assert_(_1, _2, n, ...) __assert_ ## n
+#define __assert_1(x)             __assert_2(x, #x)
+
+#if defined(__cplusplus)
+#  define __assert_2(x, message)                                        \
+    ((x) ? static_cast<void>(0) :                                       \
+     __assertion_error(message, __FILE__, __PRETTY_FUNCTION__, __LINE__))
+#else
+#  error
+#endif
+
+__attribute__((__noreturn__, __nothrow__))
+static inline
+void
+__assertion_error(const char* message, const char* file, const char* function, unsigned line)
+{
+    // TODO
+    _Exit(EXIT_FAILURE);
+}
+
+#pragma clang diagnostic pop
+
+//--------------------------------------------------------------------------------------------------
+
 #endif
