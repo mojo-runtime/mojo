fe1b80cd57e222465dd32156670ea4c1c1fecb80
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-25 09:04:07 -0400

    __todo(): need __builtin_unreachable() when preprocessed out

--- include/c/__todo.h
+++ include/c/__todo.h
@@ -4,7 +4,7 @@
 #pragma clang diagnostic ignored "-Wreserved-id-macro"
 
 #if defined(NDEBUG)
-#  define __todo(message)
+#  define __todo(message) __builtin_unreachable()
 #else
 #  include "__debug_error.h"
 #  define __todo(message) __debug_error("todo", message, __FILE__, __PRETTY_FUNCTION__, __LINE__)
