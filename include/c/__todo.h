dd5aebb084175ebde6c0e066c9daf7b0c981b40b
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-25 08:34:27 -0400

    __todo() macro

---
+++ include/c/__todo.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+
+#if defined(NDEBUG)
+#  define __todo(message)
+#else
+#  include "__debug_error.h"
+#  define __todo(message) __debug_error("todo", message, __FILE__, __PRETTY_FUNCTION__, __LINE__)
+#endif
+
+#pragma clang diagnostic pop
