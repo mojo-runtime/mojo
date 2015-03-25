63a2a9d6a60b00b47673d3d1b334569ba18c49d6
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-24 22:39:23 -0400

    Use `std::is_null_pointer` now that it exists

--- include/std/is_fundamental.hxx
+++ include/std/is_fundamental.hxx
@@ -1,17 +1,16 @@
 #pragma once
 
 #include "is_arithmetic.hxx"
+#include "is_null_pointer.hxx"
 #include "is_same.hxx"
 #include "is_void.hxx"
-#include "nullptr_t.hxx"
-#include "remove_cv_t.hxx"
 
 namespace std {
 
 template <typename T>
 struct is_fundamental : integral_constant<bool,
-                                          is_arithmetic<T>()                   ||
-                                          is_same<nullptr_t, remove_cv_t<T>>() ||
+                                          is_arithmetic<T>()   ||
+                                          is_null_pointer<T>() ||
                                           is_void<T>()>
 {
 };
--- include/std/is_scalar.hxx
+++ include/std/is_scalar.hxx
@@ -3,10 +3,9 @@
 #include "is_arithmetic.hxx"
 #include "is_enum.hxx"
 #include "is_member_pointer.hxx"
+#include "is_null_pointer.hxx"
 #include "is_pointer.hxx"
 #include "is_same.hxx"
-#include "nullptr_t.hxx"
-#include "remove_cv_t.hxx"
 
 namespace std {
 
@@ -15,8 +14,8 @@ struct is_scalar : integral_constant<bool,
                                      std::is_arithmetic<T>()     ||
                                      std::is_enum<T>()           ||
                                      std::is_member_pointer<T>() ||
-                                     std::is_pointer<T>()        ||
-                                     std::is_same<nullptr_t, remove_cv_t<T>>()>
+                                     std::is_null_pointer<T>()   ||
+                                     std::is_pointer<T>()>
 {
 };
 
