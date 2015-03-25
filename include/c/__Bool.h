57063eedcb4f5236df30ff852bcac78a975a7159
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-25 02:18:17 -0400

    `__Bool`, C99

--- include/c/__Bool.h
+++ include/c/__Bool.h
@@ -2,6 +2,12 @@
 
 #if defined(__cplusplus)
 typedef bool __Bool;
+#elif defined(__STDC_VERSION__)
+#  if __STDC_VERSION__ >= 199901L
+typedef _Bool __Bool;
+#  else
+#    error
+#  endif
 #else
 #  error
 #endif
