1a8c7f462814ac646b10bae1342de775769b56a1
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-04-17 18:24:52 -0400

    I really dropped the ball here

--- include/c/id_t.h
+++ include/c/id_t.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #if defined(__linux__)
+#  include "uint32_t.h"
 typedef uint32_t id_t;
 #elif defined(__FreeBSD__)
-#  include "uint64_t.h"
+#  include "int64_t.h"
 typedef int64_t id_t;
 #else
 #  error
