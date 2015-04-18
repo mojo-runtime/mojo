4b5b398052fcfd5a74217d06888f46afb311943b
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-04-17 20:51:39 -0400

    Remove `exit_group` to collapse some dependencies

--- include/c/_Exit.h
+++ include/c/_Exit.h
@@ -1,18 +1,28 @@
 #pragma once
 
-#if defined(__unix__)
-#  include "SYS_exit.h"
-#  include "abi/__syscall_1_no_return.h"
-#  include "compat/__static_cast.h"
-
-__noreturn
+__attribute__((__noreturn__, __nothrow__))
 static inline
 void
-_Exit(int status) __noexcept
+_Exit(int status)
 {
-    __syscall_1_no_return(SYS_exit, __static_cast(__Word, status));
-}
-
+#if defined(__x86_64__)
+#  if defined(__linux__)
+    __asm__ __volatile__ ("syscall" :: "a" (60), "D" (status));
+#  elif defined(__FreeBSD__)
+    __asm__ __volatile__ ("syscall" :: "a" (1), "D" (status));
+#  else
+#    error
+#  endif
+#elif defined(__arm__) && !defined(__thumb__)
+#  if defined(__linux__)
+    register int r0 __asm__ ("r0") = 1;
+    register int r1 __asm__ ("r1") = status;
+    __asm__ __volatile__ ("swi 0x0" :: "r" (r0), "r" (r1));
+#  else
+#    error
+#  endif
 #else
 #  error
 #endif
+    __builtin_unreachable();
+}
