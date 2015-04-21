e39db5978e695ba11f21b6deaa81ecb9bc4b88f0
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-04-20 22:17:25 -0400

    fstat error documentation

--- include/os/fstat.hxx
+++ include/os/fstat.hxx
@@ -8,33 +8,48 @@ namespace os {
 
 static inline
 auto
-fstat(int fd, struct stat* buf) noexcept
+fstat(int fd, struct stat* sb) noexcept
 {
-#define _(name) _##name = name
+#define _(name, doc) _##name = name
     enum Error
     {
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__linux__)
 #  include "c/EBADF.h"
 #  include "c/EFAULT.h"
-        _(EBADF),
-        _(EFAULT),
+
+        _(EBADF,
+          "`fd` is not a valid open file descriptor."),
+
+        _(EFAULT,
+          "`sb` points to an invalid address."),
+
 #endif
 #if defined(__FreeBSD__)
 #  include "c/EIO.h"
-        _(EIO),
+
+        _(EIO,
+          "An I/O error occurred while reading from or writing to the file system."),
+
 #endif
 #if defined(__linux__)
 #  include "c/ENOMEM.h"
-        _(ENOMEM),
+
+        _(ENOMEM,
+          "Out of kernel memory."),
+
 #endif
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__linux__)
 #  include "c/EOVERFLOW.h"
-        _(EOVERFLOW),
+
+        _(EOVERFLOW,
+          "The file size in bytes cannot be represented correctly"
+          " in the structure pointed to by `sb`."),
+
 #endif
     };
 #undef _
 
-    return Result<void, Error>(SYS_fstat, fd, buf);
+    return Result<void, Error>(SYS_fstat, fd, sb);
 }
 
 }
