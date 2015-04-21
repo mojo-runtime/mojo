66892fb5d6041829fb6d161870ed85318fb64f01
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-04-21 00:45:19 -0400

    munmap error documentation

--- include/os/munmap.hxx
+++ include/os/munmap.hxx
@@ -8,19 +8,27 @@ namespace os {
 
 static inline
 auto
-munmap(void* addr, size_t length) noexcept
+munmap(void* address, size_t length) noexcept
 {
-#define _(name) _##name = name
+#define _(name, doc) _##name = name
     enum Error
     {
 #if defined(__linux__) || defined(__FreeBSD__)
 #  include "c/EINVAL.h"
-        _(EINVAL),
+
+        _(EINVAL,
+          "`address` is not page aligned"
+          " / "
+          "`length` is zero or negative"
+          " / "
+          "Some part of the region being unmapped"
+          " is outside the valid address range for a process."),
+
 #endif
     };
 #undef _
 
-    return Result<void, Error>(SYS_munmap, addr, length);
+    return Result<void, Error>(SYS_munmap, address, length);
 }
 
 }
