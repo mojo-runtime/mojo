c87626e11ce7fb68bcb08d66b77c2c9cfda371ac
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-04-21 02:28:39 -0400

    `dev_t`: linux definition was incorrect

--- include/c/dev_t.h
+++ include/c/dev_t.h
@@ -1,8 +1,17 @@
 #pragma once
 
 #if defined(__linux__)
-#  include "uint32_t.h"
-typedef uint32_t dev_t; // XXX: this differs from `stat::st_dev`
+#  if defined(__arm__)
+#    if defined(__ARMEB__)
+typedef unsigned short dev_t;
+#    else
+typedef unsigned long dev_t;
+#    endif
+#  elif defined(__x86_64__)
+typedef unsigned long dev_t;
+#  else
+#    error
+#  endif
 #elif defined(__FreeBSD__)
 #  include "uint32_t.h"
 typedef uint32_t dev_t;
