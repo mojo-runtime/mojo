91c6eed25119484fb316da7bede00d4520708853
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-04-21 02:28:55 -0400

    `nlink_t`: linux definition was incorrect

--- include/c/nlink_t.h
+++ include/c/nlink_t.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #if defined(__linux__)
-#  include "uint32_t.h"
-typedef uint32_t nlink_t;
+#  if defined(__arm__)
+typedef unsigned int nlink_t;
+#  elif defined(__x86_64__)
+typedef unsigned long nlink_t;
+#  else
+#    error
+#  endif
 #elif defined(__FreeBSD__)
 #  include "uint16_t.h"
 typedef uint16_t nlink_t;
