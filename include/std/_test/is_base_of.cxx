a91d3a49b02d632a67e805108087644d34ac4812
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-05-07 11:26:44 -0400

    std::is_base_of

---
+++ include/std/_test/is_base_of.cxx
@@ -0,0 +1,18 @@
+#include "std/is_base_of.hxx"
+
+struct A
+{
+};
+
+struct B : A
+{
+};
+
+struct C
+{
+};
+
+static_assert( std::is_base_of<A, B>(), "");
+static_assert(!std::is_base_of<B, A>(), "");
+static_assert(!std::is_base_of<C, B>(), "");
+static_assert( std::is_base_of<C, C>(), "");
