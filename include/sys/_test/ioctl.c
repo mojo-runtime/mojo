1151787f24bc59b915e63d3000b0b787a1984db4
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-05-21 04:28:03 -0400

    <sys/ioctl.h>: non-standard, do not bother

--- include/sys/_test/ioctl.c
+++
@@ -1,7 +0,0 @@
-#include <sys/ioctl.h>
-
-int
-main()
-{
-    return 0;
-}
--- include/sys/ioctl.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#if 1 // Linux, FreeBSD
-#  include "c/TCGETS.h"
-#  include "c/TCSETS.h"
-#  include "c/TCSETSW.h"
-#  include "c/TCSETSF.h"
-#endif
