5ddc0610d8566fd2a4961e4072505a7baa874e7f
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-05-20 21:06:02 -0400

    Forgot to eat my own dog food here

--- lib/feature/__nothrow.h
+++ lib/feature/__nothrow.h
@@ -1,13 +1,6 @@
 #pragma once
 
-#if defined(__has_attribute)
-#  define __has_attribute_nothrow __has_attribute(nothrow)
-#elif defined(__GNUC__)
-#  include "__has_gnuc.h"
-#  define __has_attribute_nothrow __has_gnuc(3, 3)
-#else
-#  error
-#endif
+#include "__has_attribute_nothrow.h"
 
 #if __has_attribute_nothrow
 #  define __nothrow __attribute__((__nothrow__))
