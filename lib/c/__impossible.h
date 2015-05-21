43bd29f9f0035451a7814c608ab6ffdee138679e
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-05-20 21:48:06 -0400

    `__impossible()` back to `__unreachable()`, now that there's no name conflict

--- lib/c/__impossible.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined(DEBUG)
-#  include "__debug_error.h"
-#  define __impossible(message) __debug_error("unreachable", __FILE__, __PRETTY_FUNCTION__, __LINE__, message)
-#else
-#  include "__unreachable.h"
-#  define __impossible(message) __unreachable()
-#endif
--- lib/c/__unreachable.h
+++ lib/c/__unreachable.h
@@ -2,11 +2,11 @@
 
 #if defined(DEBUG)
 #  include "__debug_error.h"
-#  define __todo() __debug_error("todo", __FILE__, __PRETTY_FUNCTION__, __LINE__, 0)
+#  define __unreachable(message) __debug_error("unreachable", __FILE__, __PRETTY_FUNCTION__, __LINE__, message)
 #else
 #  include <feature/__has_builtin_unreachable.h>
 #  if __has_builtin_unreachable
-#    define __todo() __builtin_unreachable()
+#    define __unreachable(message) __builtin_unreachable()
 #  else
 #    error
 #  endif
